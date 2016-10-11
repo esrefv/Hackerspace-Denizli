d499b87c74804b743b95391a4898b6397914796b
esrefv <esref.viduslu@gmail.com>
2016-10-11 13:53:22 +0300

    Project editing

--- lib/jobs_helper.rb
+++ lib/jobs_helper.rb
@@ -1,9 +1,9 @@
 # Get system admin
 def system_admin
-  admin = Admin.where(email: "system@hackerspace.com").first
+  admin = Admin.where(email: "system@hacerspace.com").first
   if admin.nil?
     password = Devise.friendly_token.first(12)
-    admin = Admin.create(name: 'System', surname: 'System', email: "system@hackerspace.com", password: password, password_confirmation: password)
+    admin = Admin.create(name: 'System', surname: 'System', email: "system@hacerspace.com", password: password, password_confirmation: password)
   end
   admin
 end
